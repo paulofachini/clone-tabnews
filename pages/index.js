9b4d0778dfe2d89380db103db83204f45e7cebd4
Paulo Luiz Fachini <paulofachini@gmail.com>
2025-09-23 10:26:36 -0300

    homepage em construção.

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
 function HomePage() {
-  return <h1>Bem-vinda Carolina! ❤️</h1>;
+  return (
+    <>
+      <h1>
+        <strong>🚧 Em Construção (clone-tabnews).</strong>
+      </h1>
+      <p>
+        👷🏼<a href="https://github.com/paulofachini">@paulofachini</a>
+      </p>
+    </>
+  );
 }
 
 export default HomePage;
